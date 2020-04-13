3e248eda3003ea21b49ba789aa0167db35b0d3f1
esmaielzareh <zarehesmaiel@gmail.com>
2020-04-13 12:22:59 +0430

    Create configuration file

---
+++ config/nova-policy.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ignoration
+    |--------------------------------------------------------------------------
+    | If you need ignore the "NovaPolicy" authentication set this value to "true"
+    | 
+    |
+    */
+
+    'ignore' => false,
+];
